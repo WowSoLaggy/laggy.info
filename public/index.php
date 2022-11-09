bb6df94397aed34ec0b7b4b4afde3786dba29804
WowSoLaggy <dohxehapo@gmail.com>
2022-11-09 23:26:02 +0500

    (+) Updated home page

--- public/index.php
+++ public/index.php
@@ -1,3 +1,4 @@
 <?php
-echo('<img src="./images/wip.png" alt="Work In Progress">');
-?>
+ echo('<div><table width="100%" height="100%" align="center" valign="center"><tr><td><p style="text-align:center;"><img src="./images/wip.png" alt="Work In Progress"/></p></td></tr></table></div>');
+ ?>
+ 
